179d2161bfc631a73a4b48c7a09ceea5098260cc
davidLovesAnApi <10281235+davidLovesAnApi@users.noreply.github.com>
2018-04-30 14:19:27 +0100

    The definition and creation of nuo (literal object).

--- nuo.js
+++ nuo.js
@@ -1,4 +1,10 @@
 ;(function(){
 	"use strict";
 	
+	//	Defining the nuo literal object.
+	
+	//	Throw if its not undefined by type.
+	if( "undefined" !== typeof nuo ) throw "\"nuo\" is already defined!";
+	//	Create nuo literal object if its typeof is undefined.
+	var nuo = window.nuo = {};
 }());
